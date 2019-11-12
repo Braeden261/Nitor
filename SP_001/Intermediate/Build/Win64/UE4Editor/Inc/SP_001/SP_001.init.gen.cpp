ce33852b2b337950b773b47975afb3a00c04fabc
DeltaChrome <98hawkeye@gmail.com>
2019-11-12 13:25:12 -0500

    all the things without map updates

---
+++ SP_001/Intermediate/Build/Win64/UE4Editor/Inc/SP_001/SP_001.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSP_001_init() {}
+	UPackage* Z_Construct_UPackage__Script_SP_001()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/SP_001",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x03CB1192,
+				0x76833CB0,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
